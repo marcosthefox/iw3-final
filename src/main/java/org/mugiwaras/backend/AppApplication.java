5b9cc05230bb13603fd007dac2af6ac2e3d771a0
Marcos <aliendomarcos@gmail.com>
2023-04-07 12:06:01 -0300

    fix pass mysql

--- src/main/java/org/mugiwaras/backend/AppApplication.java
+++ src/main/java/org/mugiwaras/backend/AppApplication.java
@@ -5,17 +5,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 @SpringBootApplication
 @Slf4j
 public class AppApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("************** ADMIN PASS ***************");
+        System.out.println(BCrypt.hashpw("admin", BCrypt.gensalt()));
+        System.out.println("************** USER PASS ***************");
+        System.out.println(BCrypt.hashpw("user", BCrypt.gensalt()));
+    }
 }
