7168ee409e0a3cc5a096f0693fcbf3d4d4826c64
Marcos <aliendomarcos@gmail.com>
2023-06-22 19:59:54 -0300

    solo auth. Endpoints para autenticarse y un get dummy para checkear si el token vencio

--- src/main/java/org/mugiwaras/backend/SecurityConfiguration.java
+++ src/main/java/org/mugiwaras/backend/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests().antMatchers(HttpMethod.POST, Constants.URL_LOGIN).permitAll()
-                .antMatchers(HttpMethod.GET, Constants.URL_DUMMY).permitAll()
+//                .antMatchers(HttpMethod.GET, Constants.URL_DUMMY).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
--- src/main/java/org/mugiwaras/backend/model/persistence/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package org.mugiwaras.backend.model.persistence;
-
-import org.mugiwaras.backend.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
